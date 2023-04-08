ffa61566d4ddb93eca6a8de7af9065bdb6f67bd3
CarlosFloresCS <carlos.flores.p@utec.edu.pe>
2023-04-08 01:37:41 -0500

    Curso de introduccion a react js

---
+++ src/TodoContext/useLocalStorage.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+function useLocalStorage(itemName, initialValue) {
+  const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(true);
+  const [item, setItem] = React.useState(initialValue);
+
+  // usando estados para imaginar una llamada al backend
+  React.useEffect(() => {
+    setTimeout(() => {
+      try {
+        //- LOCAL STORAGE --
+        const localStorageItem = localStorage.getItem(itemName);
+        let parsedItem;
+
+        if (!localStorageItem) {
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItem = initialValue;
+        } else {
+          parsedItem = JSON.parse(localStorageItem);
+        }
+
+        setItem(parsedItem);
+        // ------------------
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    }, 1000);
+  });
+
+  // PERSISTENCIA DE INFORMACION CON LOCAL STORAGE
+  const saveItem = (newItem) => {
+    try {
+      const stringifiedItem = JSON.stringify(newItem);
+      localStorage.setItem(itemName, stringifiedItem);
+      setItem(newItem);
+    } catch (error) {
+      setError(error);
+    }
+  };
+
+  return {
+    item,
+    saveItem,
+    loading,
+    error,
+  };
+  // ---------------------------------------------
+}
+
+export { useLocalStorage };
