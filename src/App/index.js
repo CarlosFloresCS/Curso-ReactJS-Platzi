a151305769908a4df0812fca6173670930d0fe0b
CarlosFloresCS <carlos.flores.p@utec.edu.pe>
2023-04-08 10:36:59 -0500

    Análisis de la composición del TODO

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { TodoProvider } from "../TodoContext";
-import { AppUI } from "./AppUI";
+//import { TodoProvider } from "../TodoContext";
+//import { AppUI } from "./AppUI";
 
 //const defaultItem = [
 //  { text: "Cortar cebolla", completed: true },
@@ -10,12 +10,38 @@ import { AppUI } from "./AppUI";
 //];
 
 function App() {
+  const [state, setState] = React.useState("estado compartido");
   // ---------------------------------
   return (
-    <TodoProvider>
-      <AppUI/>
-    </TodoProvider>
+    <React.Fragment>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+
+      <TodoList>
+        <TodoItem state={state} />
+      </TodoList>
+    </React.Fragment>
   );
+}
+
+function TodoHeader({ children }) {
+  return <header>{children}</header>;
+}
+
+function TodoList({ children }) {
+  return <section className="TodoList-container">{children}</section>;
+}
+
+function TodoCounter() {
+  return <p>TodoCounter</p>;
+}
+function TodoSearch() {
+  return <p>TodoSearch</p>;
+}
+function TodoItem({ state }) {
+  return <p>TodoItem: {state}</p>;
 }
 
 export default App;
