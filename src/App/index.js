a9aeb4d23a3f5a24f80a2b28ec84a7fe5bdaac91
CarlosFloresCS <carlos.flores.p@utec.edu.pe>
2023-04-09 00:35:51 -0500

    Reemplazando por render props

--- src/App/index.js
+++ src/App/index.js
@@ -8,6 +8,9 @@ import { CreateTodoButton } from "../CreateTodoButton";
 import { Modal } from "../Modal";
 import { TodoForm } from "../TodoForm";
 import { useTodos } from "./useTodos";
+import { TodosError } from "../TodosError";
+import { TodosLoading } from "../TodosLoading";
+import { EmptyTodos } from "../EmptyTodos";
 
 //const defaultItem = [
 //  { text: "Cortar cebolla", completed: true },
@@ -39,12 +42,14 @@ function App() {
         <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       </TodoHeader>
 
-      <TodoList>
-        {error && <p>Desesperate, hubo un error...</p>}
-        {loading && <p>Estamos cargando, no desesperes</p>}
-        {!loading && !searchedTodos.length && <p>!Crea tu primer TODO!</p>}
-
-        {searchedTodos.map((todo) => (
+      <TodoList
+        error={error}
+        loading={loading}
+        searchedTodos={searchedTodos}
+        onError={() => <TodosError />}
+        onLoading={() => <TodosLoading />}
+        onEmptyTodos={() => <EmptyTodos />}
+        render={(todo) => (
           <TodoItem
             key={todo.text}
             text={todo.text}
@@ -52,8 +57,8 @@ function App() {
             onComplete={() => completeTodo(todo.text)}
             onDelete={() => deleteTodo(todo.text)}
           />
-        ))}
-      </TodoList>
+        )}
+      />
 
       {!!openModal && (
         <Modal>
