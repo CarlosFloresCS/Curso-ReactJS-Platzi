2fa99ea7c790a111d9831733494087b591ccd5af
CarlosFloresCS <carlos.flores.p@utec.edu.pe>
2023-04-12 18:13:53 -0500

    ejemplo de HOC

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App/index.js';
+//import App from './App/index.js';
 import './index.css';
 
+function App(props){
+  return (
+    <h1>¡{props.saludo}, {props.nombre}!</h1>
+  );
+}
+
+function withSaludo(saludo) {
+  return function WrappedComponentWithSaludo(WrappedComponent) {
+    return function ComponentDeVerdad(props) {
+      return (
+        <React.Fragment>
+          <WrappedComponent {...props} saludo={saludo} />
+          <p>Estamos acompañando al WrappedComponent</p>
+        </React.Fragment>
+      );
+    }
+  }
+}
+
+const AppWithSaludo = withSaludo('Wenas')(App);
+
 ReactDOM.render(
-  <App />,
+  <AppWithSaludo nombre="Carlos"/>,
   document.getElementById('root')
 );
+
+//ReactDOM.render(
+//  <App />,
+//  document.getElementById('root')
+//);
